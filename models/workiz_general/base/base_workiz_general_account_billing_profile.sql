e38f2b0361613b4cc519bd34c0112f3fdc2cc1cf
Noam Kashtan <noamkashtan@Noam-MacBook-Pro.local>
2024-06-17 16:16:41 +0300

    adding folders to models

---
+++ models/workiz_general/base/base_workiz_general_account_billing_profile.sql
@@ -0,0 +1,3 @@
+SELECT id,account_id,address,city,state,country,zip,expire_date,four_digits,csv,anet_customerProfileId,anet_customerPaymentProfileId,card_type,card_holder_first,card_holder_last,stripe_customer_id,cast(extra_balance as string) as extra_balance,TO_JSON_STRING(balance_by_type) as balance_by_type,
+updated
+FROM `workiz_general.account_billing_profile`
