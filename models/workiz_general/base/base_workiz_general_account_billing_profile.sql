5f868bce1c8b9a085e054e691b7fd50a1bf61177
Noam Kashtan <noamkashtan@Noam-MacBook-Pro.local>
2024-06-19 15:35:12 +0300

    testing new commit

--- models/workiz_general/base/base_workiz_general_account_billing_profile.sql
+++ models/workiz_general/base/base_workiz_general_account_billing_profile.sql
@@ -1,7 +1,24 @@
 with source_data as (
 
-SELECT id,account_id,address,city,state,country,zip,expire_date,four_digits,csv,anet_customerProfileId,anet_customerPaymentProfileId,card_type,card_holder_first,card_holder_last,stripe_customer_id,cast(extra_balance as string) as extra_balance,TO_JSON_STRING(balance_by_type) as balance_by_type,
-updated
+SELECT id,
+       account_id,
+       address,
+       city,
+       state,
+       country,
+       zip,
+       expire_date,
+       four_digits,
+       csv,
+       anet_customerProfileId,
+       anet_customerPaymentProfileId,
+       card_type,
+       card_holder_first,
+       card_holder_last,
+       stripe_customer_id,
+       cast(extra_balance as string) as extra_balance,
+       TO_JSON_STRING(balance_by_type) as balance_by_type,
+       updated
 from {{ source('workiz_general', 'account_billing_profile') }}  
 
 ) 
